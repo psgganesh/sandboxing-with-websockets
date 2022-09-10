d82d4868888d33bcb2f01fe329f7293ec3d3247d
Shankar <psgganesh@gmail.com>
2022-09-10 18:42:19 +0800

    Updated step3

--- README.md
+++ README.md
@@ -1,32 +1,7 @@
-# Step 2: Setting up the Javascript frontend
-The frontend is a simple javascript project written using Vite, VueJS, TailwindCSS.
-
-## Overview
-Vite - Vite (French word for "quick", pronounced /vit/, like "veet") is a build tool that aims to provide a faster and leaner development experience for modern web projects.
-VueJS - An approachable, performant and versatile framework for building web user interfaces.
-TailwindCSS - A CSS framework to rapidly build modern websites without ever leaving your HTML.
-
-#### 1. Clone this repository onto local
-> Ensure you have nodejs v16.17.0 installed on your local. 
-> You can install from the installer based on your platform present at the [official nodejs download page](https://nodejs.org/en/download/).
+# Step 3: 
+### You are all done, just run the dev server :)
 ```shell
 git clone https://github.com/psgganesh/sandboxing-with-websockets.git
-git checkout step2
-```
-
-#### 2. Install dependencies
-```shell
-npm install
-```
-#### 3. Copy the `.env.example` and paste it as `.env` file at the root of the project
-```shell
-cp .env.example .env
-```
-
-#### 4. Bring up the local server
-```shell
+git checkout step3
 npm run dev
 ```
-Open a new browser tab and navigate to `http://127.0.0.1:5173`.
-
-In the next step we will build a chat layout and start using our backend built using SAM CLI.
--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,99 @@
-<script>
-export default {
-  data() {
-    return {
-      socketConnection: null,
-      isValidSocketConnection: false
-    }
-  },
-  mounted() {
-    this.socketConnection = import.meta.env.VITE_WEB_SOCKET_API_ENDPOINT
-    this.isValidSocketConnection = this.socketConnection.includes('wss://')
-  }
-}
-</script>
-
 <template>
-  <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-    <img src="./assets/beams.jpeg" alt="background-image" class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" />
-    <div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-      <div class="mx-auto max-w-md">
-        <div class="divide-y divide-gray-300/50">
-          <div class="space-y-6 py-8 text-base leading-7 text-gray-600">
-            <ul class="space-y-4">
-              <li class="flex items-center">
-                <svg class="h-6 w-6 flex-none fill-green-100 stroke-green-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
-                  <circle cx="12" cy="12" r="11" />
-                  <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
-                </svg>
-                <p class="ml-4">
-                  Frontend tools are setup
-                </p>
-              </li>
-              <li class="flex items-center" v-if="isValidSocketConnection">
-                <svg class="h-6 w-6 flex-none fill-green-100 stroke-green-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
-                  <circle cx="12" cy="12" r="11" />
-                  <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
-                </svg>
-                <p class="ml-4">
-                  Websocket URI is set in the .env file
-                </p>
-              </li>
-              <li class="flex items-center" v-else>
-                <svg class="h-7 w-7 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clip-rule="evenodd" />
-                </svg>
-                <p class="ml-4">
-                  <code class="text-sm font-bold text-red-500">Websocket URI is not set in the .env file</code>
-                </p>
-              </li>
-            </ul>
-            <p>Perfect now you can continue to Step 3 to create an app with Chat UI</p>
-          </div>
+  <div class="flex flex-col h-screen bg-[url('./assets/memphis-colorful.png')]">
+    <div class="flex-grow bg-pattern">
+      <div class="section h-full">
+        <ol class="h-full flex flex-col justify-end align-bottom p-2">
+          <li
+              v-for="(message, index) in messages"
+              :key="index"
+              class="rounded border-2 border-gray-50 p-2 my-2 bg-amber-50"
+          >
+            {{ message.text }}
+          </li>
+        </ol>
+      </div>
+    </div>
+    <div class="border-x-0 border-solid border-slate-500 border-t-2 border-b-0 bg-white">
+      <div class="flex rounded-md shadow-sm">
+        <div class="relative flex items-stretch flex-grow focus-within:z-10 p-5">
+          <input
+              type="text"
+              class="focus:outline-none block w-full pl-2 sm:text-md bg-white"
+              placeholder="Send your message ..."
+              v-model="input"
+              @keydown.enter="send"
+          />
         </div>
+        <button
+            type="button"
+            @click="send"
+            class="inline-flex items-center px-5 py-2 border border-transparent shadow-sm text-gray-900 bg-green-50 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-100"
+        >
+          <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              class="h-5 w-5 mr-2"
+              stroke="current-color"
+          >
+            <path
+                d="M511.6 36.86l-64 415.1c-1.5 9.734-7.375 18.22-15.97 23.05c-4.844 2.719-10.27 4.097-15.68 4.097c-4.188 0-8.319-.8154-12.29-2.472l-122.6-51.1l-50.86 76.29C226.3 508.5 219.8 512 212.8 512C201.3 512 192 502.7 192 491.2v-96.18c0-7.115 2.372-14.03 6.742-19.64L416 96l-293.7 264.3L19.69 317.5C8.438 312.8 .8125 302.2 .0625 289.1s5.469-23.72 16.06-29.77l448-255.1c10.69-6.109 23.88-5.547 34 1.406S513.5 24.72 511.6 36.86z"
+            />
+          </svg>
+          <span>Send</span>
+        </button>
       </div>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      input: "",
+      messages: [],
+      socketConnection: null,
+    }
+  },
+  mounted() {
+    // Calling onconnect Lambda function via API endpoint
+    const instance = this;
+    const connection = new WebSocket(import.meta.env.VITE_WEB_SOCKET_API_ENDPOINT);
+    this.socketConnection = connection;
+    connection.onopen = function (e) {
+      console.log("onopen");
+      console.dir(e);
+    };
+    connection.onerror = function (error) {
+      console.error("onerror");
+      console.dir(error);
+    };
+    connection.onmessage = function (e) {
+      console.log("onmessage");
+      console.dir(e);
+      if (e && e.data) {
+        instance.messages.push({ text: e.data });
+      }
+    };
+    connection.onclose = function () {
+      console.log("onclose");
+    };
+  },
+  methods: {
+    // Calling sendmessage Lambda function via API endpoint
+    send: function () {
+      this.socketConnection.send(
+          JSON.stringify({
+            action: "onsend",
+            data: this.input,
+          })
+      );
+      this.input = "";
+    },
+    disconnect: function () {
+      // Calling ondisconnect Lambda function via API endpoint
+      this.socketConnection.disconnect();
+    },
+  },
+}
+</script>
